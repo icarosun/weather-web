b67eee4c152d134877a077db0e29692878323c81
Ícarosun <icaropereirasun@gmail.com>
2026-01-15 18:50:55 -0400

    Change lat and long to Brazil

--- src/components/Map/MapView.tsx
+++ src/components/Map/MapView.tsx
@@ -1,5 +1,5 @@
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 import './MapView.css';
 
@@ -9,11 +9,16 @@ interface MapViewProps {
 
 export default function MapView({ id }: MapViewProps) {
   return (
-    <MapContainer id={id} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer id={id} center={[-9.9256, -63.0714]} zoom={5} >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable. <br /> It's true
+        </Popup>
+      </Marker>
     </MapContainer>
   )
 }
