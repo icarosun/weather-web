bf2127f8b244fa4200db8d8b8be47c06f08e32f9
Ícarosun <icaropereirasun@gmail.com>
2026-01-15 17:37:31 -0400

    Mock map leaflet

---
+++ __mocks__/react-leaflet.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const MapContainer = ({ id, children }: any) => (
+  <div data-testid={id}>{children}</div>
+);
+
+export const TileLayer = () => null;
+export const Marker = () => null;
+export const Popup = () => null;
+export const useMap = () => null;
